9bdc13f993d6a6a4b3abf45fbb69657f1e540538
Aviral-Omar <f20190411@pilani.bits-pilani.ac.in>
2021-07-06 23:41:12 +0530

    switched to using a pool

--- controllers/streams.js
+++ controllers/streams.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import StreamsClient from "../util/streams_client.js";
+import StreamsPool from "../util/streams_pool.js";
 
 import Stream from "../models/stream.js";
 
@@ -19,12 +19,9 @@ export const getStreams = async (req, res, next) => {
 
 export const getStreamTable = async (req, res) => {
   const { id } = req.query;
-  console.log(id);
   const query = `SELECT * FROM public.stream_table_${id} LIMIT 100`;
   try {
-    const streamTable = await StreamsClient.query(query);
-    console.log(streamTable);
-    StreamsClient.end();
+    const streamTable = await StreamsPool.query(query);
     res.status(200).json(streamTable);
   } catch {
     res.status(502).end();
--- util/streams_pool.js
+++ util/streams_pool.js
@@ -1,21 +1,20 @@
 import pg from "pg";
 
-const { Client } = pg;
+const { Pool } = pg;
 
 const POSTGRES_URI = "postgres://ps1:vCG9gNRZNoBKM@10.0.1.84:26257/cliff";
 
-let StreamsClient;
+let StreamsPool;
 
 try {
-  StreamsClient = new Client({
+  StreamsPool = new Pool({
     connectionString: POSTGRES_URI,
     ssl: {
       rejectUnauthorized: false,
     },
   });
-  StreamsClient.connect();
 } catch (err) {
   console.log(err);
 }
 
-export default StreamsClient;
+export default StreamsPool;
